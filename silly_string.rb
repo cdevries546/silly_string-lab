41a0d036eb825a993157ab3ce42bf4367ba5aaf5
Claire de Vries <cdevries@comcast.net>
2017-08-08 15:44:03 +0000

    added a new ruby file and finished the lab

--- silly_string.rb
+++ silly_string.rb
@@ -1,4 +1,16 @@
 #code your solutions here
+puts my_word="Supercilious"
+puts my_word.upcase
+puts "IM NOT SHOUTING".downcase
+puts "Karlie Kloss".length
 
-
-
+puts "wow+mom" 
+ "i really like programming".capitalize
+ puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub("A","O").gsub("a","o")
+ puts my_word+"Chicago"
+ puts "IM NOT SHOUTING"+"Los Angeles"
+ puts "New York"+"Karlie Kloss".length
+ puts "wow+mom"+ "Saint Louis"
+ puts "Detroit"+"i really like programming".capitalize
+ put "Oakland".gsub("O","A")
+ puts "Austin".gsub("A","O")
